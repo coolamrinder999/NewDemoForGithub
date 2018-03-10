09b5786d5adb04949ae3a78fb86120f2de023623
Dell <meghasaroch44@gmail.com>
2018-03-10 18:11:42 +0530

    hello

--- src/ClassFromMegha.java
+++ src/ClassFromMegha.java
@@ -3,11 +3,12 @@ public class ClassFromMegha {
 
     static void go() {
         System.out.println("hello from megha");
-
+        System.out.println("hello from harpreet");
     }
 
     public static void main(String[] args) {
         go();
         System.out.println("in main method");
+        
     }
 }
