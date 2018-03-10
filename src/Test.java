1e7cff374c33ad9b60152e5b45a934a02dbe22d6
Dell <>
2018-03-10 17:08:29 +0530

    Committing for Test FILE

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    int empno;
+    String name;
+
+    public Test(int empno, String name) {
+        this.empno = empno;
+        this.name = name;
+    }
+    
+         
+}
